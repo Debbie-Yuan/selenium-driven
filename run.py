5e23a509bb07467df8a13a646a749577c89054f7
daipeiyuan <debbie_yuan@icloud.com>
2022-09-20 17:34:59 +0800

    1. Wrap the download & concat functionalities into a single run.py file with argparse.

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import urllib3
 import logging
 import argparse
 
-from downloader import download
+from downloader import download, concat
 from downloader.rangespec import DParts
 
 logging.basicConfig(filename="tests.log", level=logging.DEBUG)
@@ -13,12 +13,7 @@ logging.getLogger("requests").setLevel(logging.WARNING)
 logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
-base = argparse.ArgumentParser()
-base.add_argument("url")
-base.add_argument("-c", "--dparts", help="Folder or specific parts list file path.")
-
-
-def test(**kwargs):
+def download_wrapper(**kwargs):
     # Mixin
     keywords = {'path': None, 'name': None, 'headers': None, 'data': None, 'retry_timeout': 3600, 'dparts': None}
 
@@ -47,10 +42,40 @@ def test(**kwargs):
     logging.info(tf)
 
 
-if __name__ == '__main__':
-    urllib3.disable_warnings()
+def concat_wrapper(**kwargs):
+    # Arguments check
+    path = kwargs.get('path')
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"Path {path} not found.")
+    concat(path)
+
+
+def get_argparser():
     base = argparse.ArgumentParser()
-    base.add_argument("url")
-    base.add_argument("-c", "--dparts")
+
+    # Download subcommand
+    _download = base.add_subparsers()
+    download_parser = _download.add_parser("download")
+    download_parser.add_argument("url")
+    download_parser.add_argument("-c", "--dparts", help="Folder or specific parts list file path.")
+    download_parser.set_defaults(func=download_wrapper)
+
+    # Concat subcommand
+    _concat = base.add_subparsers()
+    concat_parser = _concat.add_parser("concat")
+    concat_parser.add_argument("path")
+    concat_parser.set_defaults(func=concat_wrapper)
+
+    return base
+
+
+if __name__ == '__main__':
+    base = get_argparser()
+
+    # Disable warnings
+    urllib3.disable_warnings()
+
+    # Parse args & execute
     args = base.parse_args()
-    test(**args.__dict__)
+    args.func(**args.__dict__)
+    exit(0)
